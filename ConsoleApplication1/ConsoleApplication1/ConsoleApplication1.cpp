9cf4f241feb01702a01b2f6f76a16666f448977e
gugasparotto <45372557+gugasparotto@users.noreply.github.com>
2022-10-30 23:08:49 -0300

    Update ConsoleApplication1.cpp
    vamo q vamoo

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -21,12 +21,13 @@ int main()
 	int velocidadeProgram = 1000;
 	cout << "Velocidade do programa em ms:\n";
 	cin >> velocidadeProgram;
-	cout << "até quanto vai: " << endl;
-	cout << "até 850: 1 " << endl;
-	cout << "até 2,300: 2 " << endl;
-	cout << "até 4k: 3 " << endl;
-	cout << "até 7k: 4 " << endl;
-	cout << "até 10k: 5 " << endl;
+	cout << "atÃ© quanto vai: " << endl;
+	cout << "atÃ© 850: 1 " << endl;
+	cout << "atÃ© 2,300: 2 " << endl;
+	cout << "atÃ© 4k: 3 " << endl;
+	cout << "atÃ© 7k: 4 " << endl;
+	cout << "atÃ© 10k: 5 " << endl;
+	cout << "atÃ© 350: 6 " << endl;
 	cin >> ciclo;
 	if (ciclo == 1) {
 		buyNow = 14;
@@ -42,6 +43,10 @@ int main()
 	}
 	else if (ciclo == 5) {
 		buyNow = 11120;
+	}
+	else if (ciclo == 6) {
+		buyNow = 5;
+
 	}
 
 	ofstream myfile;
@@ -62,7 +67,7 @@ int main()
 
 	int colCarta, linCarta;										  /* coluna e linha da tela                        */
 	POINT pt;											 /* para usar as coordenadas do mouse             */
-	HDC hdc = GetDC(0);									 /* HDC é o buffer da tela Toda                   */
+	HDC hdc = GetDC(0);									 /* HDC Ã© o buffer da tela Toda                   */
 	COLORREF clrf;										 /* para trabalhar com cores                      */
 	int colPergunta, linPergunta;
 	int colBan, linBan;
@@ -71,19 +76,19 @@ int main()
 	COLORREF clrf1;
 
 	POINT ptBan;											 /* para usar as coordenadas do mouse             */
-	HDC hdcBan = GetDC(0);									 /* HDC é o buffer da tela Toda                   */
+	HDC hdcBan = GetDC(0);									 /* HDC Ã© o buffer da tela Toda                   */
 	COLORREF clrfBan;										 /* para trabalhar com cores                      */
 
 
-	colCarta = 1112;
-	linCarta = 286;
+	colCarta = 1233;
+	linCarta = 508;
 
-	colPergunta = 739;
-	linPergunta = 387;
+	colPergunta = 760;
+	linPergunta = 412;
 
-	colBan = colCarta + 20;
-	linBan = linCarta - 45;
-	myfile << "Dados da execução:\n";
+	colBan = 1296;
+	linBan = 246;
+	myfile << "Dados da execuÃ§Ã£o:\n";
 
 	for (e = 0; e < quantidadeCiclo; e++)
 	{
@@ -94,147 +99,92 @@ int main()
 
 			std::cout << "\n Nova Pesquisa \n \n";
 			Sleep(150);
-			aumentarBin();
+			if (i != 0) {
+				aumentarBin();
+			}
 			std::cout << "Aumentou bid \n";
 			Sleep(velocidadeProgram);
 
 			pesquisar();
-			std::cout << "Pesquisou\n";
+			//std::cout << "Pesquisou\n";
+
+			//std::cout << "Ciclo: " << e << endl;
+			//std::cout << "Rodada: " << i << endl;
+			//std::cout << "Compras: " << compras << endl;
+			Sleep(40);
 
 
-
-
-			std::cout << "Ciclo: " << e << endl;
-			std::cout << "Rodada: " << i << endl;
-			std::cout << "Compras: " << compras << endl;
-			Sleep(150);
-
-			hdcBan = GetDC(0);
-			clrfBan = GetPixel(hdcBan, colBan, linBan);
-			int rBan = GetRValue(clrfBan);
-			int gBan = GetGValue(clrfBan);
-			int bBan = GetBValue(clrfBan);
-
-			if (rBan == 227) {
-				myfile << "Numero de ciclos: " << e << " numero de compras: " << compras << "\n";
-				if (rBan == 227)
-					cout << "Banzinho meu galo, da uma segurada.\n";
-
-				Beep(500, 500);
-				return -1;
-				Sleep(9999999);
-				Sleep(9999999);
-				Sleep(9999999);
-				return -1;
-			}
-			for (int h = 1; h < 500; h++) {
-
-				Sleep(4);
-				comprar();
-				hdc = GetDC(0);
+			bool entrei = false;
+			for (int h = 1; h < 10400; h++) {
+				
+				//hdc = GetDC(0);
 				clrf = GetPixel(hdc, colCarta, linCarta);
 				int r = GetRValue(clrf);
-				int g = GetGValue(clrf);
-				int b = GetBValue(clrf);
+				//int g = GetGValue(clrf);
+				//int b = GetBValue(clrf);
+
+
+				
+				if (r > 215) {
+					for (int z = 0; z < 4; z++) {
+						comprar();
+						std::cout << "comprou\n";
+						cout << '\a';
+						cout << r << '\a';
+					}
+					gravarTela();
+					std::cout << r;
+					cout << '\a';
+					//std::cout << b;
+					cout << '\a';
+					std::cout << colCarta;
+					cout << '\a';
+					std::cout << linCarta;
+					cout << '\a';
+					Sleep(1300);
+					compras++;
+					gravarTela();
+					std::cout << "Parou\n";
+					Sleep(4000);
+					std::cout << "Voltou\n";
+					break;
+				}
+
 				hdc1 = GetDC(0);
 				clrf1 = GetPixel(hdc1, colPergunta, linPergunta);
 				int r1 = GetRValue(clrf1);
 				int g1 = GetGValue(clrf1);
 				int b1 = GetBValue(clrf1);
+				if (b1 > 204) {
+					entrei = true;
+					voltar();
+					std::cout << "Voltando1. " << endl;
+					break;
+				}
 
+
+				Sleep(2);
+
+				//ban 
 				hdcBan = GetDC(0);
 				clrfBan = GetPixel(hdcBan, colBan, linBan);
 				int rBan = GetRValue(clrfBan);
 				int gBan = GetGValue(clrfBan);
 				int bBan = GetBValue(clrfBan);
-
-				if (h == 5) {
-					std::cout << "R: " << r << endl;
-					std::cout << "B: " << b << endl;
-					std::cout << "G: " << g << endl;
+				if (rBan > 209 && rBan < 214) {
+					
+					std::cout << "banzinho meu rei. " << endl;
+					exit(3);
+					
 				}
 
-
-				if (r == 6 || rBan == 227) {
-					myfile << "Numero de ciclos: " << e << " numero de compras: " << compras << "\n";
-					if (rBan == 227)
-					{
-						cout << "Banzinho meu galo, da uma segurada.\n";
-					}
-					else {
-						cout << "Acerta os bixo e volta. Abraços \n";
-					}
-
-					Beep(500, 500);
-					return -1;
-					Sleep(9999999);
-					Sleep(9999999);
-					Sleep(9999999);
-				}
-				if (r == 255) {
-					if (h % 2 == 0) {
-						std::cout << "acabou " << endl;
-						Beep(523, 500);
-						comprar();
-						return -1;
-						Sleep(9999999);
-						Sleep(9999999);
-						Sleep(9999999);
-						Sleep(9999999);
-						Sleep(9999999);
-						Sleep(9999999);
-					}
-				}
-				if (b1 > 220) {
-					voltar();
-					std::cout << "Voltando. " << endl;
-					h = 600;
-				}
-				else {
-					if (r > 23 && b > 40) {
-						gravarTela();
-						std::cout << r;
-						std::cout << b;
-						std::cout << colCarta;
-						std::cout << linCarta;
-						//if (b >= 71 & b <= 81) {
-							//if (g >= 64 & g <= 74) {
-						for (int z = 0; z < 2; z++) {
-							comprar();
-							std::cout << "comprou\n";
-							cout << '\a';
-							cout << r << '\a';
-
-
-
-						}
-						Sleep(1000);
-						compras++;
-						gravarTela();
-						std::cout << "Parou\n";
-						Sleep(3000);
-						std::cout << "Voltou\n";
-						h = 500;
-						//exit(0);
-
-				//}
-
-					}
-
-
-
-
-
-
-
-
-
-				}
 
 			}
 
-
-			voltar();
+			if (!entrei) {
+				std::cout << "Voltando2. " << endl;
+				voltar();
+			}
 
 		}
 
